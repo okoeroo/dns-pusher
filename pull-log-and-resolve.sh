614e69397d5e7b7a2189ad0ee1225d1c454226ae
Oscar Koeroo <okoeroo@gmail.com>
2024-10-06 18:41:24 +0200

    fix

--- pull-log-and-resolve.sh
+++ pull-log-and-resolve.sh
@@ -3,6 +3,6 @@
 LOG_EXTRACT_FILE="$(mktemp)"
 
 grep Answer /var/log/syslog | egrep 'rcode="0"' | cut -d" " -f 21-23 | sort -u > ${LOG_EXTRACT_FILE}
-python3 dns-pusher.py
+python3 dns-pusher.py --input ${LOG_EXTRACT_FILE}
 
 rm ${LOG_EXTRACT_FILE}
