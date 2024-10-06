77525c0f0f372bb1539deca6797f60345d7abf5f
Oscar Koeroo <okoeroo@gmail.com>
2024-10-06 18:26:26 +0200

    support script

---
+++ pull-log-and-resolve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+LOG_EXTRACT_FILE="$(mktemp)"
+
+grep Answer /var/log/syslog | egrep 'rcode="0"' | cut -d" " -f 21-23 | sort -u > ${LOG_EXTRACT_FILE}
+python3 dns-pusher.py
+
+rm ${LOG_EXTRACT_FILE}
